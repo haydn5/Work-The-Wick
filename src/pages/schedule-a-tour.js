8970566a7847669432f2569b40e8d37c11a4882b
Haydn Adams <haydn@beyondpapavalley.com>
2019-12-02 05:44:44 -0800

    updated tour form

--- src/pages/schedule-a-tour.js
+++ src/pages/schedule-a-tour.js
@@ -48,16 +48,25 @@ const scheduleATour = (props) => (
                     </p>
                         <Form.Group>
                             <Form.Label>Name</Form.Label>
-                            <Form.Control type="name" placeholder="John Doe" />
+                            <Form.Control 
+                                type="name"
+                                name="name"
+                                placeholder="John Doe" />
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="name@example.com" />
+                            <Form.Control 
+                                type="email" 
+                                name="email"
+                                placeholder="name@example.com" />
                         </Form.Group>
                         
                         <Form.Group>
                             <Form.Label>Example textarea</Form.Label>
-                            <Form.Control as="textarea" rows="3" />
+                            <Form.Control 
+                                as="textarea" 
+                                name="message"
+                                rows="3" />
                         </Form.Group>
                         <Button type="submit">Submit form</Button>
                         </Form>
