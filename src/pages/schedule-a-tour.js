29fd4d035ececaae8d46f3f1c4f7d919bdad632e
Haydn Adams <haydn@beyondpapavalley.com>
2019-12-02 05:18:45 -0800

    form update

--- src/pages/schedule-a-tour.js
+++ src/pages/schedule-a-tour.js
@@ -35,30 +35,15 @@ const scheduleATour = (props) => (
                     name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field"
                     >
                         <input type="hidden" name="form-name" value="contact" />
+                        <Form.Group>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="name" placeholder="John Doe" />
+                        </Form.Group>
                         <Form.Group controlId="exampleForm.ControlInput1">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="name@example.com" />
                         </Form.Group>
-                        <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Label>Example select</Form.Label>
-                            <Form.Control as="select">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="exampleForm.ControlSelect2">
-                            <Form.Label>Example multiple select</Form.Label>
-                            <Form.Control as="select" multiple>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            </Form.Control>
-                        </Form.Group>
+                        
                         <Form.Group controlId="exampleForm.ControlTextarea1">
                             <Form.Label>Example textarea</Form.Label>
                             <Form.Control as="textarea" rows="3" />
